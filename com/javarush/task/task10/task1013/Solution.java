eae04283a0d9d2bfb9516ff107458c6c8d27873a
Sergey Leuhin <l.s.d.8198@gmail.com>
2018-02-17 21:56:59 +0300

    Second commit
    Second commit

--- com/javarush/task/task10/task1013/Solution.java
+++ com/javarush/task/task10/task1013/Solution.java
@@ -9,6 +9,24 @@ public class Solution {
     }
 
     public static class Human {
-        // напишите тут ваши переменные и конструкторы
+        private static int age;
+        private static String name;
+        private static char sex;
+        private static String profession;
+        private static int weight;
+        private static int growth;
+
+        public Human (String name){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, int age){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, int age, char sex){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, char sex){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, int age, String profession){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, String profession, int weight){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, int age, int growth){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, int age, char sex, int weight){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, int age, char sex, int weight, int growth){}// напишите тут ваши переменные и конструкторы
+        public Human (String name, int age, char sex, int weight, int growth, String profession){}// напишите тут ваши переменные и конструкторы
+
+
     }
 }
